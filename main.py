270168761a8d4da3e92e0ac300c757e53e45a164
AnnelineD <anneline.daggelinckx@hotmail.com>
2022-10-17 23:20:31 +0200

    Test to read in PDDL, convert to dlplan states, and add features

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import dlplan
+from tarski.io import PDDLReader
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from src.tarski_transition_model import *
+from src.dl_transition_model import *
+from src.conversions import *
 
 
-# Press the green button in the gutter to run the script.
+def pddl_to_dlplan_states(domain_file: str, instance_file: str):
+    reader = PDDLReader(raise_on_error=True)
+    tproblem: TProblem = reader.read_problem(domain_file, instance_file)
+    tsystem = TarskiTransitionSystem(tproblem)
+
+    i = dlinstance_from_tarski(tproblem.language)
+    dlstates = tmodels_to_dlstates(tsystem.states, i)
+    dlsystem = DLTransitionModel(i, dlstates)
+    return dlsystem
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    dlsystem = pddl_to_dlplan_states("blocks_4_clear/domain.pddl", "blocks_4_clear/p-3-0.pddl")
+    factory = dlplan.SyntacticElementFactory(dlsystem.instance_info.get_vocabulary_info())
+    n = factory.parse_numerical("n_count(c_primitive(on,0))")
+    H = factory.parse_boolean("b_nullary(arm-empty)")
+    feature_sys = add_features(dlsystem, {n, H})
+
+    print(feature_sys.states)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
