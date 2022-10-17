79e318d48b6918a35b075f6d7b93c379c49d980f
AnnelineD <anneline.daggelinckx@hotmail.com>
2022-10-17 13:20:10 +0200

    Add method to add multiple features at once to a dlplan problem

--- src/dl_transition_model.py
+++ src/dl_transition_model.py
@@ -1,7 +1,9 @@
 import dlplan
 from dlplan import State as DLState
 import copy
+from typing import Union
 
+Feature = Union[dlplan.Boolean, dlplan.Numerical]
 
 class DLTransitionModel:
     def __init__(self, instance_info: dlplan.InstanceInfo, states: set[dlplan.State]):
@@ -13,16 +15,15 @@ class DLTransitionModel:
         self.n_features: list[dlplan.Numerical] = []
 
 
-def eval_feature(feature, states: list[DLState]):  # -> dict[DLState, feature]
+def eval_feature(feature, states: set[DLState]):  # -> dict[DLState, feature]
     return {s: feature.evaluate(s) for s in states}
 
 
-def add_feature_propositions(model: DLTransitionModel, feature) -> DLTransitionModel:
-    if isinstance(feature, dlplan.Boolean):
-        model.b_features.append(feature)
-    if isinstance(feature, dlplan.Numerical):
-        model.n_features.append(feature)
+def eval_features(features: set[Feature], states: set[DLState]) -> dict[DLState, dict[Feature, any]]:
+    return {s: {f: f.evaluate(s) for f in features} for s in states}
 
+
+def add_feature_propositions(model: DLTransitionModel, feature: Union[dlplan.Boolean, dlplan.Numerical]) -> DLTransitionModel:
     nv: dlplan.VocabularyInfo = copy.deepcopy(model.instance_info.get_vocabulary_info())
     nv.add_predicate(feature.compute_repr(), 1)  # TODO write more beautiful representation for features
     ni = dlplan.InstanceInfo(nv)
@@ -34,11 +35,37 @@ def add_feature_propositions(model: DLTransitionModel, feature) -> DLTransitionM
     for state in model.states:
         val = feature.evaluate(state)
         n_atom = ni.add_atom(feature.compute_repr(), [str(val)])
-        ni.add_atom(n_atom.get_predicate(), n_atom.get_objects())
         n_state = [ni.get_atom(idx) for idx in state.get_atom_idxs()]
         n_state.append(n_atom)
+        print(state)
+        print(dlplan.State(ni, n_state))
+        print()
         new_states.add(dlplan.State(ni, n_state))
 
+    return DLTransitionModel(ni, new_states)
+
+
+def add_features(model: DLTransitionModel, features: set[Feature]) -> DLTransitionModel:
+    nv: dlplan.VocabularyInfo = copy.deepcopy(model.instance_info.get_vocabulary_info())
+    for f in features:
+        nv.add_predicate(f.compute_repr(), 1)  # TODO write more beautiful representation for features
+
+    ni = dlplan.InstanceInfo(nv)
+
+    for atom in model.instance_info.get_atoms():
+        ni.add_atom(atom.get_predicate(), atom.get_objects())  # add all atoms to the new instance
+
+    feature_vals: dict[DLState, dict[Feature, any]] = eval_features(features, model.states)
+    new_states = set[dlplan.State]()
+    for state, feature_dict in feature_vals.items():
+        new_atoms = list[dlplan.Atom]()
+        n_state = [ni.get_atom(idx) for idx in state.get_atom_idxs()]  # copy the state
+        for feature, val in feature_dict.items():
+            n_atom = ni.add_atom(feature.compute_repr(), [str(val)])
+            new_atoms.append(n_atom)
+
+        n_state.extend(new_atoms)  # add feature atoms to the state
+        new_states.add(dlplan.State(ni, n_state))
     return DLTransitionModel(ni, new_states)
 
 
