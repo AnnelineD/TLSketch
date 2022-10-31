c8dbfc0c9b3017c10ffdc201676b29589c5c027e
AnnelineD <anneline.daggelinckx@hotmail.com>
2022-10-31 15:30:50 +0100

    Add function to merge rules

--- src/sketch_to_ltl.py
+++ src/sketch_to_ltl.py
@@ -3,9 +3,14 @@ import ltl
 from dlplan import Policy
 from ltl import *
 from functools import reduce
+from collections import namedtuple
+from src.rule_representation import *
 
 policy: Policy
 
+#  conditions: list[Condition]
+#  effects:  list[list[Effect]]
+RuleTupleRepr = namedtuple("Rule", "conditions effects")
 
 class NumLTL:
     def __init__(self, rules: list[tuple[LTLFormula, LTLFormula]]):
@@ -13,6 +18,7 @@ class NumLTL:
         self.conditions = [r[0] for r in self.rules]
 
 
+# FIXME the current test for this function fails
 def to_num_ltl(policy: Policy) -> NumLTL:
     curr_precondition_merge: list[tuple[list[dlplan.BaseCondition], list[list[dlplan.BaseEffect]]]] = list()
     new_precondition_merge: list[tuple[list[dlplan.BaseCondition], list[list[dlplan.BaseEffect]]]] = list()
@@ -30,6 +36,7 @@ def to_num_ltl(policy: Policy) -> NumLTL:
     print("yes", new_precondition_merge)
 
     # TODO merge rules that have independent preconditions
+    # TODO merge if all not overlapping conditions are exclusive
     ltl_rules = list[tuple[LTLFormula, LTLFormula]]()
     # for rule in policy.get_rules():
     #     cs: list[dlplan.BaseCondition] = rule.get_conditions()
@@ -43,4 +50,45 @@ def to_num_ltl(policy: Policy) -> NumLTL:
     return NumLTL(ltl_rules)
 
 
+def get_condition_features(cs: set[Condition]) -> set[Feature]:
+    return {c.feature for c in cs}
+
+
+def merge_e_n_o(r1: RuleTupleRepr, r2: RuleTupleRepr) -> list[RuleTupleRepr]:
+    #  check overlapping
+    c1: set[Condition] = set(r1.conditions)
+    c2: set[Condition] = set(r2.conditions)
+    if c1 == c2:
+        return [RuleTupleRepr(r1.conditions, r1.effects + r2.effects)]
+
+    c_intersect: set[Condition] = c1.intersection(c2)
+
+    exclusive_c1 = c1.difference(c_intersect)
+    exclusive_c2 = c2.difference(c_intersect)
+
+    excl_fs_1 = get_condition_features(exclusive_c1)
+    excl_fs_2 = get_condition_features(exclusive_c2)
+
+    if not excl_fs_1:
+        print("NOT IMPLEMENTED")
+    if not excl_fs_2:
+        print("NOT IMPLEMENTED")
+
+    f_intersect = excl_fs_1.intersection(excl_fs_2)
+
+    if not f_intersect:  # if the non overlapping conditions affect different features
+        true_c1: set[Condition] = exclusive_c1
+        true_c2: set[Condition] = exclusive_c2
+        false_c1: set[Condition] = {c.invert() for c in exclusive_c1}
+        false_c2: set[Condition] = {c.invert() for c in exclusive_c2}
+
+        #  c1 is true but c2 not
+        m1 = RuleTupleRepr(c_intersect.union(true_c1).union(false_c2), r1.effects)
+
+        # c1 if false and c2 true
+        m2 = RuleTupleRepr(c_intersect.union(false_c1).union(true_c2), r2.effects)
+
+        # c1 and c2 are true
+        m3 = RuleTupleRepr(c_intersect.union(true_c1).union(true_c2), r1.effects + r2.effects)
+        return [m1, m2, m3]
 
