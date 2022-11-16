d0a48ac666cb183d07ea7895581921c9ab73413f
AnnelineD <anneline.daggelinckx@hotmail.com>
2022-11-16 17:42:43 +0100

    Add method to convert transition system and features to smv

--- src/to_model_checker/model_check_input.py
+++ src/to_model_checker/model_check_input.py
@@ -1,23 +1,23 @@
-from src.transition_system.dl_transition_model import DLTransitionModel
+from src.transition_system.dl_transition_model import DLTransitionModel, DLFeatureTransitionModel
 from src.dlplan_utils import repr_feature
 
 
 def convert_transition_system(transition_system: DLTransitionModel):
     nl = '\n'   # f-strings cannot include backslashes
-    return f"MODULE main\nVAR " \
-        f"  state: {{{', '.join([f's{s.id}' for s in transition_system.states])}}};" \
-        f"ASSIGN" \
-        f"  init(state) := {transition_system.init_state.id};" \
-        f"  next(state) := case" \
-        f"{nl.join(f'''          state = s{s.id}: {{{ ', '.join(f's{t.id}' for t in transition_system.targets[s]) }}}''' for s in transition_system.states)};" \
-        f"                 esac"
+    return f"VAR \n" \
+        f"  state: {{{', '.join([f's{i}' for i, s in enumerate(transition_system.states)])}}};\n" \
+        f"ASSIGN \n" \
+        f"  init(state) := {transition_system.initial_state.get_index()}; \n" \
+        f"  next(state) := case \n" \
+        f"{nl.join(f'''          state = s{i}: {{{ ', '.join(f's{t}' for t in transition_system.graph.nbs(i)) }}};''' for i, s in enumerate(transition_system.states))}\n" \
+        f"                 esac;"
 
 
-def convert_features(ts: DLTransitionModel):
+def convert_features(ts: DLFeatureTransitionModel):
     tab = '\t'
     nl = '\n'
     return f"DEFINE \n " \
-    f"{nl.join(f''' {tab}{repr_feature(fn)} := case {nl + tab + tab}{(nl + tab + tab).join(f'state = s{s.get_index()}: {ts.features[fn][s]};' for s in ts.states)}''' for fn in ts.features.keys())}" \
+    f"{nl.join(f''' {tab}{repr_feature(fn)} := case {nl + tab + tab}{(nl + tab + tab).join(f'state = s{s.get_index()}: {ts.features[fn][s]};' for s in ts.transition_model.states)}''' for fn in ts.features.keys())}" \
     f"\n \t esac;"
 
 
---
+++ test/to_model_check_test.py
@@ -0,0 +1,61 @@
+import unittest
+
+import dlplan
+
+from src.to_model_checker.model_check_input import *
+from src.transition_system.graph import DirectedGraph
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        v = dlplan.VocabularyInfo()
+        v.add_predicate("raise", 0)
+
+        i = dlplan.InstanceInfo(v)
+        i.add_atom("raise", [])
+
+        atoms = i.get_atoms()
+        a0 = atoms[0]
+
+        s0 = dlplan.State(i, [a0])
+        s1 = dlplan.State(i, [])
+
+        states = [s0, s1]
+        g = DirectedGraph()
+        n0 = g.grow()
+        n1 = g.grow()
+        g.add(n0, n1, "lower")
+        g.add(n1, n0, "raise")
+
+        self.tm = DLTransitionModel(i, states, s0, g)
+
+        f = dlplan.SyntacticElementFactory(v)
+        b_feature = f.parse_boolean("b_nullary(raise))")
+        b_feature.set_index(0)
+
+        self.ftm = self.tm.add_features([b_feature])
+        print(self.ftm.features)
+
+
+    def test_something(self):
+        print(convert_transition_system(self.tm))
+        print(convert_features(self.ftm))
+        smv_var = """VAR 
+  state: {s0, s1};
+ASSIGN 
+  init(state) := 0; 
+  next(state) := case 
+          state = s0: {s1};
+          state = s1: {s0};
+                 esac;"""
+        smv_features = """DEFINE 
+  	b0 := case 
+		state = s0: True;
+		state = s1: False;
+ 	 esac;"""
+        self.assertEqual(smv_var, convert_transition_system(self.tm))
+        self.assertEqual(smv_features, convert_features(self.ftm))
+
+
+if __name__ == '__main__':
+    unittest.main()
