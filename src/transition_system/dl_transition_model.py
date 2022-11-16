06d5ad3d44e089ac7fd3fe812bcccfd03ad6cd3a
AnnelineD <anneline.daggelinckx@hotmail.com>
2022-11-16 13:25:01 +0100

    separate dl transition model from transition model with features

--- src/transition_system/dl_transition_model.py
+++ src/transition_system/dl_transition_model.py
@@ -3,28 +3,38 @@ from dlplan import State as DLState
 import copy
 from typing import Union
 
+from src.transition_system.graph import DirectedGraph
+
 Feature = Union[dlplan.Boolean, dlplan.Numerical]
 
 
 class DLTransitionModel:
-    def __init__(self, instance_info: dlplan.InstanceInfo, states: set[dlplan.State], features=None):
+    def __init__(self, instance_info: dlplan.InstanceInfo, states: list[dlplan.State], init: dlplan.State, graph: DirectedGraph):
         for s in states:
             assert(s.get_instance_info() == instance_info)
+        self.initial_state = init
+        self.graph = graph
         self.instance_info = instance_info
         self.states = states
+
+    def add_features(self, features: list[Feature]):
+        return DLFeatureTransitionModel(self, eval_features(features, self.states))
+
+
+class DLFeatureTransitionModel:
+    def __init__(self, model: DLTransitionModel, features: dict[Feature, dict[dlplan.State, Union[bool, int]]]):
+        self.transition_model = model
         self.features: dict[Feature, dict[dlplan.State, Union[bool, int]]] = features
 
 
-def eval_feature(feature, states: set[DLState]):  # -> dict[DLState, feature]
+def eval_feature(feature, states: set[DLState]) -> dict[DLState, Feature]:
     return {s: feature.evaluate(s) for s in states}
 
 
-def eval_features(features: set[Feature], states: set[DLState]) -> dict[Feature, dict[DLState, any]]:
+def eval_features(features: list[Feature], states: list[DLState]) -> dict[Feature, dict[DLState, any]]:
     return {f: {s: f.evaluate(s) for s in states} for f in features}
 
 
-def add_features2(tm: DLTransitionModel, features: set[Feature]):
-    return DLTransitionModel(tm.instance_info, tm.states, eval_features(features, tm.states))
 
 
 
--- test/dl_transition_model_test.py
+++ test/dl_transition_model_test.py
@@ -38,9 +38,9 @@ class DLTransitionModelTest(unittest.TestCase):
     s2 = dlplan.State(i, [a2, a3, a6, a7, a8])
     s3 = dlplan.State(i, [a3, a4, a7])
     s4 = dlplan.State(i, [a2, a5, a6])
-    states = {s0, s1, s2, s3, s4}
+    states = [s0, s1, s2, s3, s4]
 
-    tm = DLTransitionModel(i, states)
+    tm = DLTransitionModel(i, states, s0, DirectedGraph())
 
     f = dlplan.SyntacticElementFactory(v)
     n_feature: dlplan.Numerical = f.parse_numerical("n_count(c_primitive(clear,0))")
@@ -48,11 +48,16 @@ class DLTransitionModelTest(unittest.TestCase):
     b_feature = f.parse_boolean("b_empty(c_primitive(holding,0))")
     b_feature.set_index(0)
 
-    def test_something(self):
+    def test_add_features(self):
 
         # bool_tm = add_feature_propositions(self.tm, self.b_feature)
-        bool_tm = add_features2(self.tm, [self.b_feature])
-        print(bool_tm.states)
+        bool_tm = self.tm.add_features([self.b_feature])
+        self.assertEqual({self.b_feature: {self.tm.states[0]: True, self.tm.states[1]: True, self.tm.states[2]: True, self.tm.states[3]: False, self.tm.states[4]: False}},
+                         bool_tm.features)
+
+        num_tm = self.tm.add_features([self.n_feature])
+        self.assertEqual({self.n_feature: {self.tm.states[0]: 1, self.tm.states[1]: 1, self.tm.states[2]: 2, self.tm.states[3]: 1, self.tm.states[4]: 1}},
+                         num_tm.features)
 
 
 
