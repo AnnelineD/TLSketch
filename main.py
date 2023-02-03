9291fec246f921c9c2841e86d7541f105762a50a
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-02-03 17:35:06 +0100

    Make a generator from working sketches; check rules first

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from functools import reduce
+from typing import Iterator
 
 import dlplan
 import ltl
+from math import comb
 import pynusmv
 from tqdm import tqdm
 from examples import *
+from src.logics.conditions_effects import Condition, Effect
 from src.logics.rules import LTLSketch
-from src.sketch_generation.generation import generate_sketches, get_feature_sets, construct_feature_generator
+from src.sketch_generation.generation import generate_sketches, get_feature_sets, construct_feature_generator, \
+    generate_rules, generate_sketches_from_rules
 from src.to_smv.conversion import *
 from src.to_smv.make_smv import to_smv_format
 from src.logics.sketch_to_ltl import policy_to_arrowsketch, fill_in_rule, fill_in_rules, list_to_ruletups, \
@@ -176,35 +180,71 @@ def model_check_sketches_():
                 if check_file(directory + filename):
                     print(arrow_sketch.show() + '\n')
 
+
 def model_check_sketches_bis():
-    domain = Miconic()
+    domain = Childsnack()
     directory = "smvs/"
-    filename = "miconic_2.smv"
-    real_sketch = domain.sketch_2()
+    filename = "childsnack_2.smv"
     generator = construct_feature_generator()
 
     # (syntactic_element_factory, config.complexity, config.time_limit, config.feature_limit, config.num_threads_feature_generator, dlplan_states)  # generate features with all states of instances
-    feature_reprs = generator.generate(domain.factory(), 5, 5, 5, 5, 5, 180, 100000, 1, domain.dl_system().states)
+    feature_reprs = generator.generate(domain.factory(), 5, 5, 5, 5, 5, 180, 100000, 1, domain.dl_system.states)
 
     numerical_features = [domain.factory().parse_numerical(r, i) for i, r in enumerate(feature_reprs) if r.startswith("n_")]
     print(numerical_features)
     boolean_features = [domain.factory().parse_boolean(r, i) for i, r in enumerate(feature_reprs) if r.startswith("b_")]
 
     feature_sets = get_feature_sets(boolean_features, numerical_features, 1)
-
-    transition_str = transition_system_to_smv(domain.dl_system())
-
+    print(f"numerical: {len(numerical_features)}")
+    print(f"bools: {len(boolean_features)}")
+    transition_str = transition_system_to_smv(domain.dl_system)
+    # j = 0
+    # for s in feature_sets:
+    #    j = j+1
+    # print(j)
+    # print(comb(len(numerical_features) + len(boolean_features), 2))
+    i = 0
     for bs, ns in tqdm(feature_sets):
-        sketches = generate_sketches(bs, ns, 1)
+        rules: Iterator[tuple[list[Condition], list[Effect]]] = generate_rules(bs, ns)
+        def check_rule_file(filepath: str) -> bool:
+            pynusmv.init.init_nusmv()
+            pynusmv.glob.load_from_file(filepath=filepath)
+            pynusmv.glob.compute_model()
+            fsm = pynusmv.glob.prop_database().master.bddFsm
+            ctls = [pynusmv.glob.prop_database()[0]]
+            check = not pynusmv.mc.check_ctl_spec(fsm, ctls[0].expr)
+            pynusmv.init.deinit_nusmv()
+            return check
 
-        f_domain = domain.dl_system().add_features(bs + ns)
+        def model_check_rule(f_domain, rule: tuple[list[Condition], list[Effect]], filepath: str = None) -> bool:
+            tups = list_to_ruletups((rule,))
+            arrow_rule = ruletups_to_arrowsketch(tups)
+            ltl_rules = fill_in_rules(arrow_rule.rules, f_domain.get_feature_bounds())
+            if len(ltl_rules) > 0:
+                g = FormulaGenerator(len(ltl_rules))
+                with open(filepath, 'w') as f:
+                    ltl_specs = []
+                    ctl_specs = [g.ctl_rule_cannot_lead_into_dead()]
+                    f.write(to_smv_format(f_domain, ltl_rules, ltl_specs, ctl_specs))
+
+            return check_rule_file(filepath)
+        f_domain = domain.dl_system.add_features(bs + ns)
+        filtered_rules = filter(lambda r: model_check_rule(f_domain, r, directory + "miconic_2_r.smv"), rules)
+        #print(len(list(filtered_rules)))
+        sketches = generate_sketches_from_rules(filtered_rules, 1)
+
+
         for s in sketches:
+            i = i+1
             tups = list_to_ruletups(s)
             arrow_sketch = ruletups_to_arrowsketch(tups)
             if model_check_sketch(f_domain, arrow_sketch, directory + filename):
-                print(arrow_sketch.show() + '\n')
-
+                yield arrow_sketch
+            # print(i)
 
 
 if __name__ == '__main__':
-    model_check_sketches_bis()
+    sketches = model_check_sketches_bis()
+    sl = list(sketches)
+    for s in sl:
+        print(s.show())
