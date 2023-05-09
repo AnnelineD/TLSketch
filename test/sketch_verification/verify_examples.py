52493e8fbb5de0278910f9af3dd5f11dbfb2480d
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-05-09 15:37:29 +0200

    Add verification for real example

--- test/sketch_verification/verify_examples.py
+++ test/sketch_verification/verify_examples.py
@@ -2,6 +2,7 @@ import unittest
 from functools import reduce
 
 import ctl
+import ltl
 
 from src.logics.laws import AbstractLaw
 from src.logics.rules import Sketch
@@ -13,22 +14,54 @@ import src.file_manager as fm
 
 class VerifyExamples(unittest.TestCase):
 
-    def test_verify_sketch(self):
+    def setUp(self) -> None:
+        ltl_conditions = lambda n: [ltl.Var(f'c{i}') for i in range(n)]
+        ltl_effects = lambda n: [ltl.Var(f'e{i}') for i in range(n)]
+        self.rules = lambda n: list(zip(ltl_conditions(n), ltl_effects(n)))
+        ctl_conditions = lambda n: [ctl.Var(f'c{i}') for i in range(n)]
+        ctl_effects = lambda n: [ctl.Var(f'e{i}') for i in range(n)]
+        self.ctl_rules = lambda n: list(zip(ctl_conditions(n), ctl_effects(n)))
+        self.ltl_goal = ltl.Var('goal')
+        self.ctl_goal = ctl.Var('goal')
+
+        def follow_rules(n) -> ltl.LTLFormula:
+            return ltl.Globally(
+                (reduce(ltl.Or, [e & (ltl.Once(c)) for c, e in self.rules(n)])
+                 & reduce(ltl.Or, [c & (ltl.Finally(e)) for c, e in self.rules(n)]))
+                | reduce(ltl.Or, [(ltl.Once(c)) & (ltl.Finally(e & (
+                            reduce(ltl.Or, [cj & ltl.Finally(ej) for cj, ej in self.rules(n)]) | self.ltl_goal))) for c, e in self.rules(n)])
+                | self.ltl_goal
+            )
+
+        # G((( ∨i (ei ∧ Oci)) ∧ ( ∨i (ci ∧ F ei))) ∨ (∨i(O(ci) ∧ F ei))) → F (goal)
+        def rules_followed_then_goal(n) -> ltl.LTLFormula:
+            return ltl.Then(
+                follow_rules(n), ltl.Finally(self.ltl_goal)
+            )
+
+        def ctl_rule_cannot_lead_into_dead(n) -> ctl.CTLFormula:
+            return reduce(ctl.And, [ctl.AG(ctl.Then(c, ctl.Not(ctl.EF(ctl.And(e, ctl.Not(ctl.EF(self.ctl_goal))))))) for c, e in self.ctl_rules(n)])
+
+        def ctl_rule_can_be_followed(n) -> ctl.CTLFormula:
+            return ctl.AG(ctl.Or(ctl.Or(reduce(ctl.Or, [ctl.And(c, ctl.EF(ctl.And(e, ctl.EF(self.ctl_goal)))) for c, e in self.ctl_rules(n)]), self.ctl_goal), ctl.Not(ctl.EF(self.ctl_goal))))
+
+        self.law1 = AbstractLaw(ctl_rule_can_be_followed, True)
+        self.law2 = AbstractLaw(ctl_rule_cannot_lead_into_dead, True)
+        self.law3 = AbstractLaw(rules_followed_then_goal, True)
+
+    def verify_existing_sketch(self, domain):
         print("yes")
+        policy = domain.sketch_2()
+        sketch = Sketch.from_policy(policy)
+        features = policy.get_boolean_features() + policy.get_numerical_features()
+        feature_vals = {f.compute_repr(): [f.evaluate(s) for s in domain.dl_system.states] for f in features}
+        print(feature_vals)
+        instance = FeatureInstance(domain.dl_system.graph, domain.dl_system.initial_state, domain.dl_system.goal_states, feature_vals)
 
-        sketch = Sketch.from_policy(BlocksClear().sketch_2())
-        instance = fm.read.feature_instance("test/sketch_verification/ts_blocks_clear_p-3-0.json", "test/sketch_verification/f_blocks_clear_p-3-0.json")
+        self.assertTrue(verify_sketch(sketch, instance, [self.law1, self.law2, self.law3]))
 
-        def ctl_rule_can_be_followed(n_rules: int) -> ctl.CTLFormula:
-            ctl_conditions = [ctl.Var(f'c{i}') for i in range(n_rules)]
-            ctl_effects = [ctl.Var(f'e{i}') for i in range(n_rules)]
-            ctl_rules = list(zip(ctl_conditions, ctl_effects))
-            ctl_goal = ctl.Var('goal')
-            return ctl.AG(ctl.Or(ctl.Or(reduce(ctl.Or, [ctl.And(c, ctl.EF(ctl.And(e, ctl.EF(ctl_goal)))) for c, e in ctl_rules]), ctl_goal), ctl.Not(ctl.EF(ctl_goal))))
-
-        abstr_law = AbstractLaw(ctl_rule_can_be_followed, True)
-
-        self.assertTrue(verify_sketch(sketch, instance, [abstr_law]))
+    def test_verify_existing_sketch(self):
+        self.verify_existing_sketch(BlocksClear())
 
 
 if __name__ == '__main__':
