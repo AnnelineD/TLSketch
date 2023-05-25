18f4a86d8d233fa59a0fc7abfaa667c7b80759ed
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-05-25 16:01:19 +0200

    Fix filenames

--- src/file_manager/names.py
+++ src/file_manager/names.py
@@ -4,12 +4,12 @@ import dlplan
 
 def transition_system(i: tarski.fstrips.Problem) -> str:
     return f"{i.domain_name}/transition_systems/" \
-           f"{'_'.join([c.name for c in i.language.constants()])}_{i.goal.__repr__().replace(' ', '')}.json"
+           f"{i.name}.json"
 
 
 def feature_vals(dlstates: list[dlplan.State], string_features, factory) -> str:
     inst: dlplan.InstanceInfo = dlstates[0].get_instance_info()
-    return f"{'_'.join([c.get_name() for c in inst.get_objects()])}_{repr(inst.get_static_atoms()).replace(' ', '')}.json"
+    return f"{'_'.join([c.get_name() for c in inst.get_objects()])}_{'-'.join(str(a) for a in dlstates[0].get_atom_idxs())}_{repr(inst.get_static_atoms()).replace(' ', '')}.json"
 
 
 def feature_file(factory, x1, x2, x3, x4, x5, x6, x7, x8, dlstates: list[dlplan.State]):
--- src/run/run.py
+++ src/run/run.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import tarski.fstrips
 
@@ -15,9 +16,12 @@ from src.sketch_verification.feature_instance import FeatureInstance
 from src.sketch_verification.verify import verify_sketch
 from src.sketch_verification.laws import law1, law2, law3, simple_law, impl_law, exists_impl_law
 from tqdm import tqdm
+import src.file_manager as fm
+from src.transition_system.transition_system import TransitionSystem
 
 from src.utils.timer import timer
-
+import warnings
+warnings.filterwarnings("ignore")
 
 def run():
     domain = ts.tarski.load_domain("../../domains/miconic/domain.pddl")
@@ -58,19 +62,17 @@ def run():
             print(the_one_sketch)
 
 
-def run_on_multiple_instances():
-    directory = "../../domains/gripper/"
-    domain_file = directory + "domain.pddl"
+def run_on_multiple_instances(domain_file: str, instance_files: list[str]):
     domain = ts.tarski.load_domain(domain_file)
     dl_vocab = ts.conversions.dlvocab_from_tarski(domain.language)
     domain_name = domain.domain_name
-    instance_files = ["p-1-0.pddl", "p-2-0.pddl"]
     all_states = []
     systems = []
 
-    print("Building transition systems")
+    print("Building transition systems and reading states")
     for inst_f in tqdm(instance_files):
         instance: tarski.fstrips.Problem = ts.tarski.load_instance(domain_file, directory + inst_f)
+        instance.name = inst_f.removesuffix(".pddl")
         transition_system = ts.tarski.tarski_to_transition_system(instance)
         dlinstance = ts.conversions.dlinstance_from_tarski(domain, instance)
         dlstates = [ts.dlplan.dlstate_from_state(s, dlinstance) for s in transition_system.states]
@@ -93,9 +95,9 @@ def run_on_multiple_instances():
     bools = [f for f in filtered_features if f.startswith("b_")]
     nums = [f for f in filtered_features if f.startswith("n_")]
 
-    @cache_to_file(f"../../cache/{domain_name}/features/{'_'.join(map(str, params))}/", lambda x: x, lambda x: x, names.feature_vals)
-    @timer(f"../../cache/timers/{domain_name}/features/{'_'.join(map(str, params))}/", names.feature_vals)
-    def calculate_feature_vals(sts: list[dlplan.State], fs: list[str], fact) -> dict:
+    @cache_to_file(f"../../cache/{domain_name}/features/{'_'.join(map(str, params))}/", lambda x: x, lambda x: x, lambda x, y, z: f"{z}.json")
+    @timer(f"../../cache/timers/{domain_name}/features/{'_'.join(map(str, params))}/", lambda x, y, z: f"{z}.json")
+    def calculate_feature_vals(sts: list[dlplan.State], fs: list[str], filename) -> dict:
         fact = dlplan.SyntacticElementFactory(sts[0].get_instance_info().get_vocabulary_info()) # TODO why doesn't it work with the given factory?
         boolean_features = [fact.parse_boolean(f) for f in fs if f.startswith("b_")]
         numerical_features = [fact.parse_numerical(f) for f in fs if f.startswith("n_")]
@@ -106,23 +108,35 @@ def run_on_multiple_instances():
     for the_one_sketch in tqdm(candidate_sketches):
         checked = True
         for e, i in enumerate(instance_files):
-            # print("looping", e)
-            feature_vals = calculate_feature_vals(all_states[e], filtered_features, factory)
+            feature_vals = calculate_feature_vals(all_states[e], filtered_features, i.removesuffix(".pddl"))
             feature_instance = FeatureInstance(systems[e].graph, systems[e].init, systems[e].goals, feature_vals)
 
-            checked = verify_sketch(the_one_sketch, feature_instance, [law1, law2, impl_law, exists_impl_law])
+            checked = verify_sketch(the_one_sketch, feature_instance, [law1, law2, exists_impl_law, impl_law])
             if not checked:
-                continue
+                break
         if checked:
+            print(the_one_sketch.rules)
+            print()
             yield the_one_sketch
 
 
 if __name__ == '__main__':
-    filename = "test.json"
+    domain_name = "blocks_4_clear"
+    directory = f"../../domains/{domain_name}/"
+    domain_file = directory + "domain.pddl"
 
-    @timer("../../generated/timers/gripper/", lambda: filename)
+    files = os.listdir(directory)
+    files.sort()
+    instance_files = list(filter(lambda x: x.startswith('p-'), files))
+    # instance_files.remove("domain.pddl")
+    # instance_files.remove("domain-with-fix.pddl")
+    # instance_files.remove("README")
+
+    filename = "10_instances_5_5_10_10_10_180_100000_1_1_1.json"
+
+    @timer(f"../../generated/timers/{domain_name}/", lambda: filename)
     def write_all():
-        with open("../../generated/gripper/" + filename, "w") as f:
-            json.dump([s.serialize() for s in run_on_multiple_instances()], f)
+        with open(f"../../generated/{domain_name}/" + filename, "w") as f:
+            json.dump([s.serialize() for s in run_on_multiple_instances(domain_file, instance_files[0:10])], f)
 
     write_all()
