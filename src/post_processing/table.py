c99f1cdb9a955bf624a2f0060a0cda904f667316
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-06-17 23:31:45 +0200

    Correct complexities

--- src/post_processing/table.py
+++ src/post_processing/table.py
@@ -3,9 +3,8 @@ from numpy import round
 
 
 def get_table(domains: list[tuple[str, int]]):
-    heads = ["", "#I", "C", "#F", "#CS", "#GS",
-             "#Timed out",
-             "avg time failed", "avg time good", "total time"]
+    heads = ["", "$\\vert I \\vert$", "C", "$\\vert F \\vert $", "$\\vert CS \\vert$", "$\\vert GS \\vert$",
+             "$\\overline{t_{failed}}$", "$\\overline{t_{good}}$", "$\\overline{t_{good}}/I$", "$t_{total}$"]
 
     def time_rounding(t):
         return round(t/1_000_000_000, 2)
@@ -13,17 +12,16 @@ def get_table(domains: list[tuple[str, int]]):
     def get_line(name, compl):
         data = get_data(name, compl, 1,1)
         return [name, data["n_instances"], compl, data["n_features"], data["n_tested"], data["n_working"],
-                data["n_timed_out"],
-                time_rounding(data["avg_time_not_working_ns"]), time_rounding(data["avg_time_working_ns"]), time_rounding(data["total_time"])]
+                time_rounding(data["avg_time_not_working_ns"]), time_rounding(data["avg_time_working_ns"]), time_rounding(data["avg_time_working_ns"]/data["n_instances"]), time_rounding(data["total_time"])]
 
     lines = [' & '.join(map(str, get_line(name, compl))) for name, compl in domains]
 
-    return ' & '.join(heads) + "\\\ \n" \
+    return ' & '.join(heads) + "\\\ \hline \n" \
         + '\\\ \n'.join(lines)
 
 
 def get_data(domain_name, complexity, num_f, num_rules) -> dict:
-    with open(f"../../generated/{domain_name}/{'_'.join([str(complexity)]*5)}_180_10000_{num_f}/rules_{num_rules}.json") as f:
+    with open(f"../../generated_final/{domain_name}/{'_'.join([str(complexity)]*5)}_180_10000_{num_f}/rules_{num_rules}.json") as f:
         info = json.load(f)
         n_working = len(info["working"])
         n_tested = info["number_tested"]
@@ -59,11 +57,11 @@ def get_data(domain_name, complexity, num_f, num_rules) -> dict:
 if __name__ == '__main__':
     domains = [("blocksworld", 4),
                ("blocksworld-on", 4),
-               # ("child-snack", 4),
+               ("child-snack", 6),
                ("delivery", 5),
                ("gripper-strips", 4),
-               ("miconic", 4),
-               ("reward-strips", 4),
+               ("miconic", 2),
+               ("reward-strips", 2),
                ("spanner", 6),
-               ("grid-visit-all", 4)]
+               ("grid-visit-all", 2)]
     print(get_table(domains))
