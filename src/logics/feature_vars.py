0989e76227ad6386d4afece0f200c3c62410ef7f
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-09-22 21:35:11 +0200

    Documentation

--- src/logics/feature_vars.py
+++ src/logics/feature_vars.py
@@ -1,7 +1,7 @@
+# Classes to represent variables based on features that can be used in logical expressions
+
 from dataclasses import dataclass
 from typing import Union, Literal
-
-import dlplan
 from ltl import Var
 
 Operator = Literal[">", "<", "="]
@@ -9,6 +9,10 @@ Operator = Literal[">", "<", "="]
 
 @dataclass(frozen=True, eq=True)
 class FeatureVar(Var):
+    """
+    A base class for boolean and numerical feature variables
+    A feature variable logical statement of the form 'data > n', 'data = n' or 'data < n', with 'data' a feature
+    """
     data: str
     value: Union[int, bool]
     operator: Operator
@@ -16,20 +20,34 @@ class FeatureVar(Var):
 
 @dataclass(frozen=True, eq=True)
 class NumericalVar(FeatureVar):
+    """
+    A feature variable in which 'data' is a numerical feature
+    """
     data: str
     value: int
 
     def var_show(self) -> str:
+        """
+        Method to convert feature variables to an easily readable and printable string
+        :return: Compact human-readable representation of the form 'data operator value' (e.g. x<5)
+        """
         return f"{self.data}{self.operator}{self.value}"
 
 
 @dataclass(frozen=True, eq=True)
 class BooleanVar(FeatureVar):
+    """
+    A feature variable in which 'data' is a boolean feature
+    """
     data: str
     value: bool
-    operator = "="
+    operator = "="   # A boolean feature can only be compared to truth values
 
     def var_show(self) -> str:
+        """
+        Method to convert feature variables to an easily readable and printable string
+        :return: Compact human-readable representation of the form 'data operator value' (e.g. x or !x)
+        """
         match self.value:
             case True: return f"{self.data}"
             case False: return f"!{self.data}"
