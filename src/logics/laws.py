7d00995744b43cf8cbbf854d55329323b4912c3e
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-09-22 21:49:55 +0200

    Documentation

--- src/logics/laws.py
+++ src/logics/laws.py
@@ -1,9 +1,14 @@
+# Law and Abstract Law classes. Laws are Temporal Logic formulas that either should hold or should not hold.
+
 from typing import Callable, Union
 from ltl import LTLFormula
 from ctl import CTLFormula
 
 
 class Law:
+    """
+    Either an LTL or CTL formula that needs to be true or false
+    """
     formula: Union[LTLFormula, CTLFormula]
     truth: bool
 
@@ -13,8 +18,13 @@ class Law:
 
 
 class AbstractLaw:
-    law: Callable[[int], Union[LTLFormula, CTLFormula]]
-    truth: bool
+    """
+    A law in which it is still uncertain how many variables there will be
+    e.g. an abstract law can be V_i (x_i) which can be expanded into the law x_0 v x_1 v x_2
+    """
+    law: Callable[[int], Union[LTLFormula, CTLFormula]]     # method that takes an integer n and expands a logical
+                                                            # formula such that a formula with n variables is returned
+    truth: bool     # whether the law needs to hold or needs to be false
 
     def __init__(self, law: Callable[[int], Union[LTLFormula, CTLFormula]], truth: bool):
         self.law = law
