ab4d2950c3b9d0df750e76600fd44cd02dc9ca0c
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-09-25 17:56:32 +0200

    Documentation

--- src/transition_system/dlplan.py
+++ src/transition_system/dlplan.py
@@ -2,50 +2,38 @@ import dlplan
 from dlplan.core import State as DLState
 from typing import Union
 
-from src.transition_system.graph import DirectedGraph
 from src.transition_system.transition_system import StateStr
 
-Feature = Union[dlplan.core.Boolean, dlplan.core.Numerical]
-
-"""
-class DLTransitionModel:
-    def __init__(self, instance_info: dlplan.InstanceInfo, states: list[dlplan.State], init: int, goal_state_indices: list[int], graph: DirectedGraph):
-        for i, s in enumerate(states):
-            assert(s.get_instance_info() == instance_info)
-            s.set_index(i)
-        self.initial_state = init
-        self.graph = graph
-        self.instance_info = instance_info
-        self.states = states
-        self.goal_states = goal_state_indices
-
-    def add_features(self, features: list[Feature]):
-        return DLFeatureTransitionModel(self, eval_features(features, self.states))
+DLFeature = Union[dlplan.core.Boolean, dlplan.core.Numerical]
 
 
-class DLFeatureTransitionModel:
-    def __init__(self, model: DLTransitionModel, features: dict[Feature, dict[dlplan.State, Union[bool, int]]]):
-        self.transition_model = model
-        self.features: dict[Feature, dict[dlplan.State, Union[bool, int]]] = features
-
-    def get_feature_bounds(self) -> dict[dlplan.Numerical, int]:
-        bounds = dict[dlplan.Numerical, int]()
-        for f, ss in self.features.items():
-            if isinstance(f, dlplan.Numerical):
-                vals: list[int] = [v for v in ss.values()]
-                bounds.update({f: max(vals)})
-        return bounds
-
-"""
-def eval_feature(feature, states: set[DLState]) -> dict[DLState, Feature]:
+def eval_feature(feature: DLFeature, states: set[DLState]) -> dict[DLState, any]:
+    """
+    Calculate the value of a feature for a set of states
+    :param feature:
+    :param states:
+    :return: A dictionary with for each state the value of the feature in that state
+    """
     return {s: feature.evaluate(s) for s in states}
 
 
-def eval_features(features: list[Feature], states: list[DLState]) -> dict[Feature, dict[DLState, any]]:
+def eval_features(features: list[DLFeature], states: list[DLState]) -> dict[DLFeature, dict[DLState, any]]:
+    """
+    Calculate the values features for a set of states
+    :param features:
+    :param states:
+    :return: A dictionary with for each feature, for each state the value of that feature in that state
+    """
     return {f: {s: f.evaluate(s) for s in states} for f in features}
 
 
 def dlstate_from_state(state: StateStr, instance: dlplan.core.InstanceInfo) -> DLState:
+    """
+    Translate a state represented as a string into a DLPlan State object
+    :param state: A state represented as a set of strings in which each string is a predicate
+    :param instance: DLPlan instance info which contains all predicates of the instance
+    :return: The same state as a DLPlan State object
+    """
     return dlplan.core.State(instance, [instance.get_atom(atom) for atom in state])
 
 
