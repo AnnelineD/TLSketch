204b222f76dbd20eb696955f59d404f70a3cf7a9
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-09-25 18:15:11 +0200

    Documentation

--- src/transition_system/__init__.py
+++ src/transition_system/__init__.py
@@ -1,5 +1,5 @@
 from .tarski import load_instance, load_domain
-from .dlplan import Feature
+from .dlplan import DLFeature
 from .conversions import dlinstance_from_tarski # , tarski_to_dl_system
 from .types import *
 
--- src/transition_system/graph.py
+++ src/transition_system/graph.py
@@ -1,29 +1,54 @@
+# A representation of a directed, edge labeled graph.
+# Made with the help of Adam Vandervorst
+
 from bisect import bisect_right
 
 EL = any
 
 
 class DirectedGraph:
+    """
+    Represent a directed, edge labeled graph with an adjacency matrix. In the adjacency matrix, for each node a list
+    of integers indicates with what other nodes it is connected.
+    """
     def __init__(self, adjacency_graph: list[list[int], list[EL]] = None):
         if adjacency_graph:
             for ns, ls in adjacency_graph:
-                assert(len(ns) == len(ls))
+                assert(len(ns) == len(ls))      # the number of edges and edge labels needs to be the same
             self.adj = adjacency_graph
         else:
             self.adj = list[list[int], list[EL]]()
 
     def size(self) -> int:
+        """
+        :return: the number of nodes the graph has
+        """
         return len(self.adj)
 
     def grow(self) -> int:
+        """
+        Add a node to the graph
+        :return: The index of the new node
+        """
         self.adj.append((list[int](), list[EL]()))
         return len(self.adj) - 1
 
     def nbs(self, i: int) -> list[int]:
+        """
+        :param i: Node index
+        :return: All neighbours of node i
+        """
         assert(i < len(self.adj))
         return self.adj[i][0]
 
     def add(self, i: int, j: int, l: EL) -> bool:
+        """
+        Add an edge between two nodes
+        :param i: the node from which to draw an edge
+        :param j: the node to which to draw an edge
+        :param l: the edge label
+        :return: False if the connection already existed, else True
+        """
         assert(i < len(self.adj))
         existing = self.adj[i]
         j_idx = bisect_right(existing[0], j)
@@ -35,6 +60,11 @@ class DirectedGraph:
         return True
 
     def show(self, statelabels=None) -> str:
+        """
+        Generate the graphviz code of the graph which can be used to make an image of the graph.
+        :param statelabels: Optional a label can be added for each state
+        :return: Graphviz code as a string
+        """
         trans = ""
         if not statelabels:
             statelabels = [i for i in range(self.size())]
