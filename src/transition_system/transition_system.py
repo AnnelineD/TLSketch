2bf8deb855283930de41dc4fef50688e18110e96
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-09-27 17:37:21 +0200

    Documentation

--- src/transition_system/transition_system.py
+++ src/transition_system/transition_system.py
@@ -1,9 +1,16 @@
+# This file defines the TransitionSystem and GraphSystem classes
+
 from src.transition_system.graph import DirectedGraph
 
-StateStr = set[str]
+StateStr = set[str]     # represent a state as a set of propositions (strings)
 
 
 class TransitionSystem:
+    """
+    Representation of a transition system containing the graph of the system, the states that label the nodes of the
+    graph, an initial state which is represented as the index of state in the states list, and the goal states which are
+    also represented as indices.
+    """
     states: list[StateStr]
     graph: DirectedGraph
     init: int
@@ -16,7 +23,8 @@ class TransitionSystem:
         self.goals = goals
 
     @classmethod
-    def deserialize(cls, data: dict):
+    def deserialize(cls, data: dict) -> 'Self':
+        """ Create TransitionSystem object from json readable object. Necessary for cashing."""
         assert("states" in data.keys())
         assert("graph" in data.keys())
         assert("init" in data.keys())
@@ -25,10 +33,15 @@ class TransitionSystem:
         return cls([set(s) for s in data["states"]], graph, data["init"], data["goals"])
 
     def serialize(self) -> dict:
+        """ Convert information from TransitionSystem object into a json readable object. Necessary for cashing."""
         return {"init": self.init, "goals": self.goals, "graph": self.graph.adj, "states": [list(s) for s in self.states]}
 
 
 class GraphSystem:
+    """
+    A GraphSystem contains graph of a transition system, together with the ordered states that label the nodes of the
+    graph.
+    """
     states: list[StateStr]
     graph: DirectedGraph
 
@@ -38,11 +51,13 @@ class GraphSystem:
 
     @classmethod
     def deserialize(cls, data: dict):
+        """ Create GraphSystem object from json readable object. Necessary for cashing."""
         assert("states" in data.keys())
         assert("graph" in data.keys())
         graph = DirectedGraph(data["graph"])
         return cls([set(s) for s in data["states"]], graph)
 
     def serialize(self) -> dict:
+        """ Convert information from Graphsystem object into a json readable object. Necessary for cashing."""
         return {"graph": self.graph.adj, "states": [list(s) for s in self.states]}
 
