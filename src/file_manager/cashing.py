a08328ca4239a12784fa0e761999998ae01b600c
AnnelineD <anneline.daggelinckx@hotmail.com>
2023-10-03 12:53:28 +0200

    Documentation

--- src/file_manager/cashing.py
+++ src/file_manager/cashing.py
@@ -1,9 +1,23 @@
+# This file was written with the help of Adam Vandervorst
+
 import json
 import os
 from os import path
 
 
 def cache_to_file(filepath: str, serializer, deserializer, namer):
+    """
+    Caches a slow function F, saving its output to a file using a provided serializer-deserializer pair,
+    hashing the inputs (for retrieval) into filenames via namer.
+    :param filepath: Path to the directory in which the cache file will be saved
+    :param serializer: Function O -> J that converts the output O of the function F you want to cache to a json writable
+                       object J, without loss of information.
+    :param deserializer: Function J -> O that is the inverse of the serializer function, taking a json readable object
+                         J and constructing the cached functions F original output O.
+    :param namer: Injective function that takes as input the arguments of F and outputs a unique filename that will be
+                  used to write or retreive F's output.
+    :return: ???
+    """
     def wrapper(f):
         def cached_f(*args, **kwargs):
             filename = namer(*args, **kwargs)
